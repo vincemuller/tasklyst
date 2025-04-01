3e2db0cf197f0eb8ec67ffa60b77416d4bf58728
vincemuller <vmuller2529@gmail.com>
2025-04-01 00:04:16 -0700

    Updated custom color names so there was no conflicts with components
    Built list(s) data models

--- tasklyst/Homescreen.swift
+++ tasklyst/Homescreen.swift
@@ -10,92 +10,71 @@ import SwiftUI
 struct HomeScreen: View {
     
     @State private var searchTerms: String = ""
+    @State private var lists: [Lists] = [Lists(name: "Weekly Grocery List", listItems: []),
+                                         Lists(name: "Disney Vacation Prep", listItems: []),
+                                         Lists(name: "Weekend To Do's", listItems: []),
+                                         Lists(name: "House Cleaning", listItems: []),]
     
     var body: some View {
-        VStack {
-            Text("tasklyst")
-                .font(.system(.title2, design: .monospaced, weight: .semibold))
-                .foregroundStyle(.accent)
-            Rectangle()
-                .fill(.accent)
-                .frame(width: 300, height: 1)
-            ZStack {
-                RoundedRectangle(cornerRadius: 8)
-                    .fill(Color(.secondary))
-                    .stroke(Color(.accent))
-                HStack {
-                    Image(systemName: "magnifyingglass")
-                        .foregroundStyle(.black.opacity(0.5))
-                    TextField("Search lists", text: $searchTerms)
-                    Spacer()
+        NavigationStack {
+            VStack {
+                Text("tasklyst")
+                    .font(.system(.title2, design: .monospaced, weight: .semibold))
+                    .foregroundStyle(.tasklystAccent)
+                Rectangle()
+                    .fill(.tasklystAccent)
+                    .frame(width: 300, height: 1)
+                ZStack {
+                    RoundedRectangle(cornerRadius: 8)
+                        .fill(.tasklystSecondary)
+                        .stroke(.tasklystAccent)
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundStyle(.black.opacity(0.5))
+                        TextField("Search lists", text: $searchTerms)
+                        Spacer()
+                    }
+                    .padding(.leading)
                 }
-                .padding(.leading)
-            }
-            .frame(width: 330, height: 40)
-            .padding()
+                .frame(width: 330, height: 40)
+                .padding()
                 Text("My Lists")
                     .font(.system(.title3, design: .monospaced, weight: .semibold))
-                    .foregroundStyle(Color(.accent))
+                    .foregroundStyle(.tasklystAccent)
                     .frame(width: 330, alignment: .leading)
-            ZStack {
-                RoundedRectangle(cornerRadius: 15)
-                    .fill(Color(.secondary))
-                    .stroke(Color(.accent))
-                List {
-                    HStack {
-                        Image(systemName: "list.bullet.circle")
-                            .foregroundStyle(Color(.accent))
-                        Text("Weekly Grocery List")
-                            .font(.system(size: 14))
-                        Spacer()
-                        Text("12")
+                ZStack {
+                    RoundedRectangle(cornerRadius: 15)
+                        .fill(.tasklystSecondary)
+                        .stroke(.tasklystAccent)
+                    List(lists) { list in
+                        NavigationLink(destination: Text(list.name)) {
+                            HStack {
+                                Image(systemName: "list.bullet.circle")
+                                    .foregroundStyle(.tasklystAccent)
+                                Text(list.name)
+                                    .font(.system(size: 14))
+                                Spacer()
+                                Text(list.getMockData().count.description)
+                                    .font(.system(size: 14))
+                            }
+                        }
+                        .listRowBackground(Color.clear)
+                        .listRowInsets(.init(top: 5, leading: 15, bottom: 5, trailing: 15))
                     }
-                    .listRowBackground(Color.clear)
-                    .listRowInsets(.init(top: 5, leading: 15, bottom: 5, trailing: 15))
-                    HStack {
-                        Image(systemName: "list.bullet.circle")
-                            .foregroundStyle(Color(.accent))
-                        Text("Weekly Grocery List")
-                            .font(.system(size: 14))
-                        Spacer()
-                        Text("12")
-                    }
-                    .listRowBackground(Color.clear)
-                    .listRowInsets(.init(top: 5, leading: 15, bottom: 5, trailing: 15))
-                    HStack {
-                        Image(systemName: "list.bullet.circle")
-                            .foregroundStyle(Color(.accent))
-                        Text("Weekly Grocery List")
-                            .font(.system(size: 14))
-                        Spacer()
-                        Text("12")
-                    }
-                    .listRowBackground(Color.clear)
-                    .listRowInsets(.init(top: 5, leading: 15, bottom: 5, trailing: 15))
-                    HStack {
-                        Image(systemName: "list.bullet.circle")
-                            .foregroundStyle(Color(.accent))
-                        Text("Weekly Grocery List")
-                            .font(.system(size: 14))
-                        Spacer()
-                        Text("12")
-                    }
-                    .listRowBackground(Color.clear)
-                    .listRowInsets(.init(top: 5, leading: 15, bottom: 5, trailing: 15))
+                    .listStyle(.plain)
                 }
-                .listStyle(.plain)
+                .frame(width: 340)
+                Spacer()
+                Button {
+                    print("create new list")
+                } label: {
+                    Image(systemName: "plus.circle")
+                        .foregroundStyle(.tasklystAccent)
+                        .font(.system(size: 40, weight: .semibold))
+                }
+                .padding(.top)
+                
             }
-            .frame(width: 340)
-            Spacer()
-            Button {
-                print("create new list")
-            } label: {
-                Image(systemName: "plus.circle")
-                    .foregroundStyle(Color(.accent))
-                    .font(.system(size: 40, weight: .semibold))
-            }
-            .padding(.top)
-
         }
     }
 }
---
+++ tasklyst/Models.swift
@@ -0,0 +1,32 @@
+//
+//  Models.swift
+//  tasklyst
+//
+//  Created by Vince Muller on 3/31/25.
+//
+
+import Foundation
+
+
+struct Lists: Identifiable {
+    var id = UUID()
+    var name: String
+    var listItems: [ListItem]
+    
+    func getMockData() -> [ListItem] {
+        return [ListItem(description: "Whole milk", completed: false),
+                ListItem(description: "Loaf of bread", completed: false),
+                ListItem(description: "Yogurt", completed: false),
+                ListItem(description: "Breakfast sausage", completed: false),
+                ListItem(description: "Raspberries", completed: false),
+                ListItem(description: "Strawberries", completed: false),
+                ListItem(description: "Blueberries", completed: false)]
+    }
+    
+}
+
+struct ListItem: Identifiable {
+    var id = UUID()
+    var description: String
+    var completed: Bool
+}
